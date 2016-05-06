f2c4b0b5a4ec1dfd38ea21c75f8abf838bcfce6c
Ranran15 <rrui1510@gmail.com>
2016-05-06 02:38:42 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-	//'url' : 'mongodb://localhost/passport-demo'
-	'url' : 'mongodb://for498webfinal:for498webfinal@ds019471.mlab.com:19471/498final'
+	'url' : 'mongodb://localhost/passport-demo'
+	//'url' : 'mongodb://for498webfinal:for498webfinal@ds019471.mlab.com:19471/498final'
 };
